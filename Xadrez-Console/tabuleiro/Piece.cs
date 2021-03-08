eb7de4947dabf7aa386cf4d0f3d3a7bb8b5e7357
Ricardo Pinheiro <ricardopacheco.pinheiro@hotmail.com>
2021-03-08 19:16:21 +0000

    Começar a implementar o tabuleiro

---
+++ Xadrez-Console/tabuleiro/Piece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    class Piece
+    {
+        public Posicao posicao { get; set; }
+        public Color color { get; protected set; }
+        public int qntMovements { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Piece(Posicao posicao, Color color, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.color = color;
+            this.tab = tab;
+            this.qntMovements = 0;
+        }
+    }
+}
