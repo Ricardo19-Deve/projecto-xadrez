b8568507d7b36b4a22b61553e17dc84d749f5018
Ricardo Pinheiro <ricardopacheco.pinheiro@hotmail.com>
2021-03-09 19:18:13 +0000

    A melhorar a implementação do método imprimirTabuleiro

--- Xadrez-Console/Tela.cs
+++ Xadrez-Console/Tela.cs
@@ -13,6 +13,7 @@ namespace xadrez_console
         {
             for (int i=0; i<tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j=0; j<tab.colunas; j++)
                 {
                     if (tab.piece(i, j)==null)
@@ -21,11 +22,28 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(tab.piece(i, j) + " ");
+                        printPiece(tab.piece(i, j));
+                        Console.Write("- ");
                     }
                 }
 
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void printPiece(Piece piece)
+        {
+            if ( piece.color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
